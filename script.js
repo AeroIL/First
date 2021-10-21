3fee9a37adb44180f4b21043972555c6b28eade5
The Michel <przeusil@gmail.com>
2021-10-21 15:19:33 +0300

    yolo v2

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Ma Omer?")
+console.log("Ma Omer?");
+console.log("Beseder");
+const title = document.querySelector("h1");
+const words = document.querySelector(".words");
+const colors = document.querySelector(".colors");
+
+words.addEventListener("click", function (e) {
+ title.textContent += " Tov ya Sharmuta"   
+});
