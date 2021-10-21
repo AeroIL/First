f2db4901d0676a8bf03a12521d8f404f866f751f
The Michel <przeusil@gmail.com>
2021-10-21 15:26:37 +0300

    yolo v3

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ console.log("Beseder");
 const title = document.querySelector("h1");
 const words = document.querySelector(".words");
 const colors = document.querySelector(".colors");
-
 words.addEventListener("click", function (e) {
- title.textContent += " Tov ya Sharmuta"   
+ title.textContent += " Tov"   
 });
+colors.addEventListener("click",function (e) {
+    title.style.color = title.style.color== "white" ? "black" : "white"
+})
